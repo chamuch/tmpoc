4d3296e66b952a6105cdc4f8d008fd10a2b1c121
Sathya Narayanan Srinivasan <sathya.narayanan.s@hotmail.com>
2016-01-25 14:41:40 +0530

    corrected xml attriute annotations

--- tmpoc-api/src/main/java/com/ericsson/tm/prodcat/simple/entities/Price.java
+++ tmpoc-api/src/main/java/com/ericsson/tm/prodcat/simple/entities/Price.java
@@ -1,5 +1,28 @@
 package com.ericsson.tm.prodcat.simple.entities;
 
+import javax.xml.bind.annotation.XmlAttribute;
+
 public class Price {
 
+	private String isoCurrencyCode = null;
+	private long amount = -1;
+	
+	@XmlAttribute (name="isoCurrencyCode")
+	public String getIsoCurrencyCode() {
+		return isoCurrencyCode;
+	}
+	
+	public void setIsoCurrencyCode(String isoCurrencyCode) {
+		this.isoCurrencyCode = isoCurrencyCode;
+	}
+	
+	@XmlAttribute (name="amount")
+	public long getAmount() {
+		return amount;
+	}
+	public void setAmount(long amount) {
+		this.amount = amount;
+	}
+	
+	
 }
